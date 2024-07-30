efe2f1edef9dd22d1ee7f22e427eb13b4c964112
garyDav <gary.2810.dav@gmail.com>
2024-07-30 19:02:53 -0400

    test: conexión a la base de datos SQL Server

--- server/EnvManager.ts
+++ server/EnvManager.ts
@@ -1,4 +1,7 @@
+import dotenv from "dotenv";
 import { studlyCaseToSnakeCase } from "./utils/functions";
+
+dotenv.config();
 
 class EnvManager {
   [x: string]: any;
--- server/database/connection.ts
+++ server/database/connection.ts
@@ -1,21 +1,39 @@
 import { Sequelize } from "sequelize-typescript";
-import { MsSqlDialect } from "@sequelize/mssql";
+import Debug from "debug";
 
 import EnvManager from "../EnvManager";
 
+process.env.DEBUG_COLORS = "true";
+
+const debug = Debug("app:database");
+const dError = Debug("app:error");
+dError.color = "1";
+
 const sequelize = new Sequelize({
-  dialect: MsSqlDialect,
-  server: EnvManager.getDbHost(),
-  port: EnvManager.getDbPort(),
+  dialect: "mssql",
   database: EnvManager.getDbName(),
-  authentication: {
-    type: "default",
+  username: EnvManager.getDbUsername(),
+  password: EnvManager.getDbPassword(),
+  host: EnvManager.getDbHost(),
+  port: EnvManager.getDbPort(),
+  dialectOptions: {
     options: {
-      userName: EnvManager.getDbUsername(),
-      password: EnvManager.getDbPassword(),
+      encrypt: true, // Utiliza el cifrado de SQL Server
+      trustServerCertificate: true, // Solo para desarrollo
     },
   },
   models: [__dirname + "/models"],
 });
+
+async function testConnection() {
+  try {
+    await sequelize.authenticate();
+    debug("Connection has been established successfully.");
+  } catch (error) {
+    dError(`Unable to connect to the database: ${error}`);
+  }
+}
+
+testConnection();
 
 export default sequelize;
