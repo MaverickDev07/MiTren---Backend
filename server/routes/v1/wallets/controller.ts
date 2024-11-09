4ab67755857d9a31549b41458ed475a48dd90529
garyDav <gary.2810.dav@gmail.com>
2024-11-09 11:24:55 -0400

    wallet: refactory random prices

--- server/routes/v1/wallets/controller.ts
+++ server/routes/v1/wallets/controller.ts
@@ -4,6 +4,15 @@ import { NextFunction, Request, Response } from 'express'
 import WalletResource from '../../../resources/WalletResource'
 import WalletRepository from '../../../repositories/WalletRepository'
 
+// Definimos el arreglo inicial de precios
+let prices: number[] = [
+  0.5, 8.5, 14.9, 6.8, 25.3, 3.2, 18.9, 4.7, 22.1, 11.4, 9.6, 15.8, 7.3, 20.2, 16.4, 5.9, 12.2,
+  24.7, 10.1, 19.8, 8.1, 13.5, 6.2, 21.9, 4.8, 17.6, 9.3, 23.4, 11.7, 15.1, 7.9, 26.5,
+]
+
+// Definimos el arreglo vacío para almacenar los precios eliminados
+let pricesBuffer: number[] = []
+
 export const listWallets = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const repository = new WalletRepository()
@@ -19,15 +28,25 @@ export const listWallets = async (req: Request, res: Response, next: NextFunctio
   }
 }
 
-const generarNumeroAleatorio = (min: number, max: number): number => {
-  return Math.floor(Math.random() * (max - min + 1)) + min
+const generarNumeroAleatorio = (): number => {
+  if (prices.length === 0) {
+    ;[prices, pricesBuffer] = [pricesBuffer, prices]
+  }
+  // Generamos un índice aleatorio
+  const randomIndex: number = Math.floor(Math.random() * prices.length)
+
+  // Eliminamos el elemento en el índice aleatorio de 'prices' y lo agregamos a 'pricesBuffer'
+  const result = prices.splice(randomIndex, 1)
+  pricesBuffer.push(...result)
+
+  return result[0]
 }
 
 export const getPriceTicket = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const data = {
       id: new Types.ObjectId().toHexString(),
-      price: generarNumeroAleatorio(11, 49),
+      price: generarNumeroAleatorio(),
       currency: 'BOB',
       payment_methods: 'EFECTIVO',
       createdAt: new Date(),
